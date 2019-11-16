6cc14169dd3c1738adfc2c98af4e5e5fc34485af
rxyan216 <rxyan216@gmail.com>
2019-11-15 21:50:18 -0800

    Add data activity.

---
+++ app/src/main/java/org/erhs/stem/project/time_management/DataActivity.java
@@ -0,0 +1,16 @@
+package org.erhs.stem.project.time_management;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
