b2340904a96d89152b040395b3d625683269c6fd
rxyan216 <rxyan216@gmail.com>
2019-11-26 04:45:16 -0800

    Refine main activity.

--- app/src/main/java/org/erhs/stem/project/time_management/activity/MainActivity.java
+++ app/src/main/java/org/erhs/stem/project/time_management/activity/MainActivity.java
@@ -1,48 +1,56 @@
 package org.erhs.stem.project.time_management.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import org.erhs.stem.project.time_management.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnData;
-    private Button btnSettings;
-    private TextView textView;
-    private ImageButton button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnData = findViewById(R.id.btnData);
-        btnSettings = findViewById(R.id.btnSettings);
-        textView = findViewById(R.id.txtTitle);
-        btnData.setOnClickListener(new View.OnClickListener() {
+        Toolbar topBar = findViewById(R.id.top_bar);
+        setSupportActionBar(topBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageButton ibStatistics = findViewById(R.id.action_statistics);
+        ibStatistics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(MainActivity.this, DataActivity.class);
-                //startActivity(intent);
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                textView.setText(String.valueOf(pref.getInt("sounds", 0)));
 
             }
         });
-        btnSettings.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton ibSettings = findViewById(R.id.action_settings);
+        ibSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button btnEnd = findViewById(R.id.action_end);
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageButton ibAdd = findViewById(R.id.action_add);
+        ibAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
             }
         });
     }
