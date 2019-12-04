f2c755535576ecda0c4040e25ed111fe6eb35141
rxyan216 <rxyan216@gmail.com>
2019-12-03 21:42:33 -0800

    Add waste time bar chart.

--- app/src/main/java/org/erhs/stem/project/time_management/activity/StatisticsChartActivity.java
+++ app/src/main/java/org/erhs/stem/project/time_management/activity/StatisticsChartActivity.java
@@ -1,12 +1,18 @@
 package org.erhs.stem.project.time_management.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -21,12 +27,14 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StatisticsChartActivity extends AppCompatActivity {
 
-    private PieChart chart;
+    private PieChart pieChart;
+    private BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +43,27 @@ public class StatisticsChartActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.action_statistics);
 
-        chart = findViewById(R.id.chart);
-        chart.getDescription().setEnabled(false);
+        createPieChart();
+        createBarChart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void createPieChart() {
+        pieChart = findViewById(R.id.pie_chart);
+        pieChart.getDescription().setEnabled(false);
 
         Calendar from = Calendar.getInstance();
-        from.add(Calendar.DATE, -7);
+        from.add(Calendar.DATE, -6);
 
         Calendar to = Calendar.getInstance();
 
@@ -65,19 +89,67 @@ public class StatisticsChartActivity extends AppCompatActivity {
                         PieDataSet pieDataSet = new PieDataSet(pieEntries, getString(R.string.last_week));
                         pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
-                        chart.setData(new PieData(pieDataSet));
+                        pieChart.setData(new PieData(pieDataSet));
                     }
                 });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    private void createBarChart() {
+        barChart = findViewById(R.id.bar_chart);
+        barChart.getDescription().setEnabled(false);
+
+        Calendar from = Calendar.getInstance();
+        from.add(Calendar.DATE, -7);
+
+        Calendar to = Calendar.getInstance();
+
+        EventRepository
+                .getEventsByDateRange(getApplicationContext(), from.getTime(), to.getTime())
+                .observe(this, new Observer<List<Event>>() {
+                    @Override
+                    public void onChanged(List<Event> events) {
+                        Map<Long, Long> wasteByDate = new HashMap<>();
+                        for (Event event : events) {
+                            if (event.actualStart != null && event.actualEnd != null) {
+                                long plannedElapsed = event.plannedEnd.getTime() - event.plannedStart.getTime();
+                                long actualElapsed = event.actualEnd.getTime() - event.actualStart.getTime();
+                                if (actualElapsed > plannedElapsed) {
+                                    Calendar calendar = Calendar.getInstance();
+                                    calendar.setTime(event.plannedStart);
+                                    calendar.set(Calendar.HOUR_OF_DAY, 0);
+                                    calendar.set(Calendar.MINUTE, 0);
+                                    calendar.set(Calendar.SECOND, 0);
+                                    calendar.set(Calendar.MILLISECOND, 0);
+
+                                    long key = calendar.getTimeInMillis();
+
+                                    if (!wasteByDate.containsKey(key)) {
+                                        wasteByDate.put(key, 0L);
+                                    }
+                                    wasteByDate.put(key, wasteByDate.get(key) + actualElapsed - plannedElapsed);
+                                }
+                            }
+                        }
+                        List<BarEntry> barEntries = new ArrayList<>();
+                        for (int i = 6; i >= 0; i--) {
+                            Calendar calendar = Calendar.getInstance();
+                            calendar.set(Calendar.HOUR_OF_DAY, 0);
+                            calendar.set(Calendar.MINUTE, 0);
+                            calendar.set(Calendar.SECOND, 0);
+                            calendar.set(Calendar.MILLISECOND, 0);
+                            calendar.add(Calendar.DATE, -i);
+
+                            long key = calendar.getTimeInMillis();
+                            if (wasteByDate.containsKey(key)) {
+                                barEntries.add(new BarEntry(calendar.get(Calendar.DAY_OF_YEAR), wasteByDate.get(key) / 60000));
+                            } else {
+                                barEntries.add(new BarEntry(calendar.get(Calendar.DAY_OF_YEAR), 0));
+                            }
+                        }
+
+                        BarDataSet barDataSet = new BarDataSet(barEntries, getString(R.string.last_week));
+                        barChart.setData(new BarData(barDataSet));
+                    }
+                });
     }
 }
